2b87e8c7ada60c03975d14341e899ee91c5da4d9
cssbruno <bruno.cs.soares@gmail.com>
2026-09-15 06:43:05 -0300

    Avoid JBR optimizing compiler crash in host unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,11 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "12.3.0" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
 }
+
+subprojects {
+    tasks.withType<org.gradle.api.tasks.testing.Test>().configureEach {
+        // JBR 21.0.10 can crash in C2 Node::uncast while compiling Robolectric code.
+        // Keep host tests on C1 with enough code cache; Android runtime code is unaffected.
+        jvmArgs("-XX:TieredStopAtLevel=1", "-XX:ReservedCodeCacheSize=512m")
+    }
+}
